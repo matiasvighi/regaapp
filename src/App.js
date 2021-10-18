8a2d6a5074c3c96719d78621f926fd2c3247c5f5
Matias <matias.vighi@gmail.com>
2021-10-18 15:25:52 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import About from './pages/About';
+import { AppBar, Button, IconButton, Toolbar, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+function App() {
+  const classes = useStyles();
+  return (
+    <Router>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Link to="/">
+            <Button>Home</Button>
+          </Link>
+          <Link to="/about">
+            <Button>About</Button>
+          </Link>
+          <Link to="/users">
+            <Button>Users</Button>
+          </Link>
+        </Toolbar>
+      </AppBar>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About/>
+          </Route>
+          <Route path="/users">
+            <p>USERS</p>
+          </Route>
+          <Route path="/">
+            <p>HOME</p>
+          </Route>
+        </Switch>
+    </Router>
+  );
+}
+
+export default App;
